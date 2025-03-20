3b5db5f8c8aac72015cb173d27b4d3b7a80f05d2
shurj0 <shurjo.sm@gmail.com>
2025-03-20 07:31:59 +0600

    int stage

--- badmadness.py
+++ badmadness.py
@@ -18,6 +18,65 @@ starting_bracket = [{"name": "one", "seed": 1},
                     {"name": "two", "seed": 2},
                     {"name": "fifteen", "seed": 15}]
 
+def get_stats(first_seed, second_seed):
+    global matchup_stats
+    index = (first_seed - 1) * 16 + (second_seed - 1)
+    return matchup_stats[index]
+
+def pick_winner_by_seed(a, b):
+    first_seed = a["seed"]
+    second_seed = b["seed"]
+    stats = get_stats(first_seed, second_seed)
+    decrementer = 1
+    while stats[5] == "NaN":
+        stats = get_stats(first_seed, second_seed - decrementer)
+        decrementer += 1
+    pct = float(stats[5])
+    if pct >= 0.985:
+        pct = 0.985
+    elif pct <= 0.015:
+        pct = 0.015
+    return a if random() < pct else b
+    
+class Bracket():
+    bracket = []
+    matchup_stats = []
+
+    def progress_bracket(self, pick_winner):
+        last_round_winners = list.copy(self.bracket)
+        while len(last_round_winners) > 1:
+            i = 0 
+            winners = []
+            while i < (len(last_round_winners) / 2):
+                winners.append(pick_winner(last_round_winners[i], last_round_winners[i+1]))
+                i += 2
+            for winner in winners:
+                self.bracket.append(winner)
+            last_round_winners = list.copy(winners)
+
+    def __init__(self, bracket, matchup_stats, pick_winner):
+        self.bracket = list.copy(bracket)
+        self.first_round_size = len(bracket)
+        self.matchup_stats = matchup_stats
+        self.progress_bracket(pick_winner)
+
+    def print_bracket(self):
+        print(f"{self.bracket[0]["name"]}\t\t")
+        print(f"{self.bracket[1]["name"]}\t\t")
+        print(f"{self.bracket[2]["name"]}\t\t")
+        print(f"{self.bracket[3]["name"]}\t\t")
+        print(f"{self.bracket[4]["name"]}\t\t{self.bracket[16]["name"]}\t\t")
+        print(f"{self.bracket[5]["name"]}\t\t{self.bracket[17]["name"]}\t\t")
+        print(f"{self.bracket[6]["name"]}\t\t{self.bracket[18]["name"]}\t\t{self.bracket[24]["name"]}\t\t")
+        print(f"{self.bracket[7]["name"]}\t\t{self.bracket[19]["name"]}\t\t{self.bracket[25]["name"]}\t\t{self.bracket[28]["name"]}\t\t{self.bracket[30]["name"]}\t\t")
+        print(f"{self.bracket[8]["name"]}\t\t{self.bracket[20]["name"]}\t\t{self.bracket[26]["name"]}\t\t{self.bracket[29]["name"]}\t\t")
+        print(f"{self.bracket[9]["name"]}\t\t{self.bracket[21]["name"]}\t\t{self.bracket[27]["name"]}\t\t")
+        print(f"{self.bracket[10]["name"]}\t\t{self.bracket[22]["name"]}\t\t")
+        print(f"{self.bracket[11]["name"]}\t\t{self.bracket[23]["name"]}\t\t")
+        print(f"{self.bracket[12]["name"]}\t\t")
+        print(f"{self.bracket[13]["name"]}\t\t")
+        print(f"{self.bracket[14]["name"]}\t\t")
+        print(f"{self.bracket[15]["name"]}\t\t")
 
 
 def load_csv():
@@ -29,54 +88,11 @@ def load_csv():
             matchup_stats.append(line)
     return matchup_stats
 
-def get_stats(first_seed, second_seed, matchup_stats):
-    index = (first_seed - 1) * 16 + (second_seed - 1)
-    return matchup_stats[index]
-        
-def flip_coin(first_seed, second_seed, matchup_stats):
-    stats = get_stats(first_seed, second_seed, matchup_stats)
-    decrementer = 1
-    while stats[5] == "NaN":
-        stats = get_stats(first_seed, second_seed - decrementer, matchup_stats)
-        decrementer += 1
-    pct = float(stats[5])
-    if pct >= 0.985:
-        pct = 0.985
-    elif pct <= 0.015:
-        pct = 0.015
-    return 1 if random() < pct else 0
-
-def progress_bracket(bracket, matchup_stats):
-    i = 0
-    return_bracket = list.copy(bracket)
-    while i < (len(bracket) / 2):
-        first_seed = return_bracket[i]["seed"]
-        second_seed = return_bracket[i+1]["seed"]
-        return_bracket.pop(flip_coin(first_seed, second_seed, matchup_stats) + i)
-        i += 1
-    return return_bracket
-
-def play_out_bracket(bracket, matchup_stats):
-    first_round = bracket
-    second_round = progress_bracket(first_round, matchup_stats)
-    third_round = progress_bracket(second_round, matchup_stats)
-    fourth_round = progress_bracket(third_round, matchup_stats)
-    winner = progress_bracket(fourth_round, matchup_stats)
-    print("{}	    {}	    {}	    {}	    {}".format(first_round[0]["name"], second_round[0]["name"], third_round[0]["name"], fourth_round[0]["name"], winner[0]["name"]))
-    print("{}	    {}	    {}	    {}".format(first_round[1]["name"], second_round[1]["name"], third_round[1]["name"], fourth_round[1]["name"]))
-    print("{}	    {}	    {}".format(first_round[2]["name"], second_round[2]["name"], third_round[2]["name"]))
-    print("{}	    {}	    {}".format(first_round[3]["name"], second_round[3]["name"], third_round[3]["name"]))
-    print("{}	    {}".format(first_round[4]["name"], second_round[4]["name"]))
-    print("{}	    {}".format(first_round[5]["name"], second_round[5]["name"]))
-    print("{}	    {}".format(first_round[6]["name"], second_round[6]["name"]))
-    print("{}	    {}".format(first_round[7]["name"], second_round[7]["name"]))
-    for i in range (8, 16):
-        print(first_round[i]["name"])
-
-        
 def main():
     matchup_stats = load_csv()
-    play_out_bracket(starting_bracket, matchup_stats)
+    sim_bracket = Bracket(starting_bracket, matchup_stats)
+    sim_bracket.print_bracket()
+
 
 if __name__ == "__main__":
     main()
